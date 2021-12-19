025dff7d09641d448b37269daad87c44a46d08bb
Elghoul Seifeddine <elghoulseifeddine@gmail.com>
2021-12-19 20:23:27 +0100

    add authentification route

--- Controllers/clientController.js
+++ Controllers/clientController.js
@@ -1,7 +1,8 @@
 const Client = require("../Models/Client");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-
+// ************************************ Add client *************************************
 
 exports.addClient = async (req, res) => {
     const newClient = new Client({ ...req.body });
@@ -22,5 +23,37 @@ exports.addClient = async (req, res) => {
     } catch (error) {
       console.log(error);
       res.status(401).json({ msg: "Register Failed" });
+    }
+  };
+
+  // ************************************ login client *************************************
+
+
+  exports.clientLogin = async (req, res) => {
+    const { email, password } = req.body;
+  
+    const client = await Client.findOne({ email });
+  
+    if (!client) return res.status(401).json({ msg: "Bad credentiel" });
+  
+    const isMatch = await bcrypt.compare(password, client.password);
+  
+    if (!isMatch) return res.status(401).json({ msg: "Bad credentiel" });
+  
+    try {
+      const payload = {
+        id: client._id,
+        firstName: client.firstName,
+        lastName: client.lastName,
+        email: client.email,
+        role: client.role
+      };
+  
+      const token = await jwt.sign(payload, process.env.secretOrPrivateKey);
+  
+      res.status(200).json({ token: `Bearer ${token}` });
+    } catch (err) {
+      console.log(err)
+      res.status(400).json({ errors: err });
     }
   };
--- Routes/client.js
+++ Routes/client.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { addClient } = require("../Controllers/clientController");
-
+const { addClient, clientLogin } = require("../Controllers/clientController");
+const { registerRules, validator } = require("../middelwares/validator");
+const isAuth = require("../middelwares/passport-setup");
 const Router = express.Router();
 
-Router.post("/addClient", addClient);
+Router.post("/addClient",registerRules(),validator ,addClient);
+Router.post("/login", clientLogin);
+Router.get("/currentClient", isAuth(), (req, res) => res.send(req.user));
 // Router.put("/updatePerson/:id", personUpdate);
 // Router.delete("/deletePerson/:id", personDelete);
 // Router.get("/getPerson", personGet);
