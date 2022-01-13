943786212c8e467282d04114bc9a324401aaca5a
Elghoul Seifeddine <elghoulseifeddine@gmail.com>
2022-01-13 12:12:06 +0100

    deployment

--- Server.js
+++ Server.js
@@ -13,6 +13,13 @@ const app= express();
 connectDB();
 
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 // app.use("/client", client);
 // app.use("/artist", artist);
 // app.use("/profile", profile);
